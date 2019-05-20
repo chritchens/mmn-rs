8171cb70ff16c4acba9de2e736cbf8f3465b6c93
chritchens <chritchens@gmail.com>
2019-05-20 17:48:00 +0200

    Functional single-threaded build.rs

--- build.rs
+++ build.rs
@@ -1,12 +1,12 @@
 use curl::easy;
+use zip::ZipArchive;
 use std::path::{Path, PathBuf};
 use std::env;
-use std::fs;
+use std::fs::{self, File};
+use std::io::{Cursor, Read, Write};
 
 /// `DEFAULT_DATA_DIR` is the default directory for the dataset(s).
 const DEFAULT_DATA_DIR: &str = "data";
-/// `TIFU_DATASET_ARCHIVE_FILE` is the TIFU dataset archive filename.
-const TIFU_DATASET_ARCHIVE_FILE: &str = "tifu_all_tokenized_and_filtered.zip";
 /// `TIFU_DATASET_FILE` is the TIFU dataset filename.
 const TIFU_DATASET_FILE: &str = "tifu_all_tokenized_and_filtered.json";
 /// `TIFU_DATASET_URL` is the TIFU dataset url.
@@ -43,36 +43,6 @@ fn build_data_dir<P: AsRef<Path>>(data_dir: P) {
     fs::create_dir(data_dir).unwrap()
 }
 
-/// `tmp_dir_path` builds the `PathBuf` of the temporary directory.
-fn tmp_dir_path() -> PathBuf {
-    // TODO
-    PathBuf::from("tmp")
-}
-
-/// `tmp_tifu_dataset_archive_file_path` builds the `PathBuf` of the temporary TIFU dataset archive.
-fn tmp_tifu_dataset_archive_file_path<P: AsRef<Path>>(data_dir_path: P) -> PathBuf {
-    let mut tmpdir = tmp_dir_path();
-    tmpdir.push(data_dir_path);
-    tmpdir.push(TIFU_DATASET_ARCHIVE_FILE);
-    tmpdir
-}
-
-/// `tmp_tifu_dataset_file_path` builds the `PathBuf` of the temporary TIFU dataset.
-fn tmp_tifu_dataset_file_path<P: AsRef<Path>>(data_dir_path: P) -> PathBuf {
-    let mut tmpdir = tmp_dir_path();
-    tmpdir.push(data_dir_path);
-    tmpdir.push(TIFU_DATASET_FILE);
-    tmpdir
-}
-
-/// `tifu_dataset_archive_file_path` builds the `PathBuf` of the TIFU dataset archive.
-fn tifu_dataset_archive_file_path<P: AsRef<Path>>(data_dir_path: P) -> PathBuf {
-    let mut data_dir = PathBuf::new();
-    data_dir.push(data_dir_path);
-    data_dir.push(TIFU_DATASET_ARCHIVE_FILE);
-    data_dir
-}
-
 /// `tifu_dataset_file_path` builds the `PathBuf` of the TIFU dataset.
 fn tifu_dataset_file_path<P: AsRef<Path>>(data_dir_path: P) -> PathBuf {
     let mut data_dir = PathBuf::new();
@@ -101,44 +71,59 @@ fn fetch_tifu_dataset_archive() -> Vec<u8> {
     archive_data
 }
 
+/// `extract_tifu_dataset_archive` extracts the TIFU dataset archive.
+fn extract_tifu_dataset_archive(archive_buf: &[u8]) -> Vec<u8> {
+    let mut tifu_dataset = Vec::new();
+    let reader = Cursor::new(archive_buf);
+    let mut zip_archive = ZipArchive::new(reader).unwrap();
+    let mut zip_file = zip_archive.by_name(TIFU_DATASET_FILE).unwrap();
+    zip_file.read_to_end(&mut tifu_dataset).unwrap();
+    tifu_dataset
+}
+
 fn main() {
     println!("build.rs starting...");
 
     println!("checking for $DATA_DIR variables, defaulting to $DEFAULT_DATA_DIR if absent...");
+    
     let data_dir = data_dir_from_env();
     let data_dir_path = data_dir_path(&data_dir);
     let tifu_dataset_file_path = tifu_dataset_file_path(&data_dir);
-    println!("data directory at {}", data_dir_path.display());
-    println!("tifu dataset at {}", tifu_dataset_file_path.display());
+  
+    println!("data directory path set at '{}'", data_dir_path.display());
+    println!("tifu dataset file path set at '{}'", tifu_dataset_file_path.display());
 
-    println!("checking if the data directory already exists");
+    println!("checking if the data directory already exists...");
+    
     if dir_exists(&data_dir_path) {
-        println!("checking if the TIFU dataset file already exists");
+        println!("the data directory already exists...");
+        println!("checking if the TIFU dataset file already exists...");
+
         if file_exists(&tifu_dataset_file_path) {
+            println!("the TIFU dataset already exists");
             return;
         }
+
+        println!("the TIFU dataset does not exist");
     } else {
-        println!("building the data directory");
+        println!("the data directory does not exist");
+        println!("building the data directory...");
+    
         build_data_dir(data_dir);
     }
 
     println!("fetching the TIFU dataset archive...");
+    
     let tifu_dataset_archive = fetch_tifu_dataset_archive();
-    println!("tifu dataset archive in MB: {}", tifu_dataset_archive.len() / 1000000);
 
-    println!("writing the TIFU dataset archive to tmp/{}", TIFU_DATASET_ARCHIVE_FILE);
-    // TODO
+    println!("extracting the TIFU dataset archive...");
+    
+    let tifu_dataset = extract_tifu_dataset_archive(&tifu_dataset_archive);
 
-    println!("extracting the TIFU dataset archive to tmp/{}...", TIFU_DATASET_FILE);
-    // TODO
-
-    println!("writing the TIFU dataset in $DATA_DIR/{}...", TIFU_DATASET_FILE);
-    // TODO
-    /*
-    let tifu_dataset_zipped_path = Path::from(data_dir)
-        .join(TIFU_DATASET_ARCHIVED_FILE);
-    let file = File::create(tifu_dataset_zipped_path).unwrap();
-    */
+    println!("writing the TIFU dataset into '{}'...", tifu_dataset_file_path.display());
+    
+    let mut tifu_dataset_file = File::create(tifu_dataset_file_path).unwrap();
+    tifu_dataset_file.write_all(&tifu_dataset).unwrap();
 
     println!("build.rs terminated.")
 }
